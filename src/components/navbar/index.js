e6ac40d194533f5743b7ce97aae8cfc8002d4321
Rafael <markesxd@gmail.com>
2021-10-25 13:02:06 -0300

    finish application

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,10 +1,16 @@
 import style from './style.module.scss';
+import {useUserContext} from '../../context/userContext.js';
 
 const Navbar = () => {
+
+  const {getRepos, getStarred, user} = useUserContext();
+
+  const username = user.login;
+
   return (
     <div className={style.container}>
-      <button>Repositories</button>
-      <button>Starred</button>
+      <button onClick={() => {getRepos(username)}}>Repositories</button>
+      <button onClick={() => {getStarred(username)}}>Starred</button>
     </div>
   )
 };
--- src/components/repoCard/index.jsx
+++ src/components/repoCard/index.jsx
@@ -1,24 +1,22 @@
+import {useEffect} from 'react';
 import style from './style.module.scss';
 import {useUserContext} from '../../context/userContext.js';
 
 const RepoCard = () => {
 
   const {repos} = useUserContext();
-
-  const handleRepos = () => {
-      return repos.map((repo, i) => {
-        return(
-          <div key={i} className={style.container}>
-            <h4>{repo.name}</h4>
-            <span>full name:</span>
-            <a href={repo.url}>{repo.fullName}</a>
-          </div>
-        )
-      })
-  }
+  const repoCards = repos.map((repo, i) => {
+    return(
+      <div key={i} className={style.card}>
+        <h4>{repo.name}</h4>
+        <span>full name:</span>
+        <a href={repo.url}>{repo.fullName}</a>
+      </div>
+    )
+  })
 
   return (
-    {handleRepos()}
+    <div className={style.container}>{repoCards}</div>
   )
 }
 
